f0868aa37f321fbee6fe3c3ec38d94ce64086468
Abhishek-901247 <mtripathi496@gmail.com>
2026-06-25 10:06:46 -0700

    Update Question2.java

--- Week_2/Day_2/Question2.java
+++ Week_2/Day_2/Question2.java
@@ -1,1 +1,44 @@
+ class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
 
+        if (n > m) {
+            return false;
+        }
+
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+
+        // Frequency of s1 and first window of s2
+        for (int i = 0; i < n; i++) {
+            count1[s1.charAt(i) - 'a']++;
+            count2[s2.charAt(i) - 'a']++;
+        }
+
+        if (matches(count1, count2)) {
+            return true;
+        }
+
+        // Sliding window
+        for (int i = n; i < m; i++) {
+            count2[s2.charAt(i) - 'a']++;          // add new character
+            count2[s2.charAt(i - n) - 'a']--;      // remove old character
+
+            if (matches(count1, count2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean matches(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
