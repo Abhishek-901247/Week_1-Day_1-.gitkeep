f58d23989ff5a04df828d7447cfad68115e33cde
Abhishek-901247 <mtripathi496@gmail.com>
2026-07-04 11:17:20 -0700

    Update Question1.java

--- Week_2/Day_6/Question1.java
+++ Week_2/Day_6/Question1.java
@@ -1,1 +1,86 @@
+1. Playable.java (Package: music)
 
+                                 package music;
+
+public interface Playable {
+    void play();
+}
+
+
+2. Veena.java (Package: music.string)
+
+
+                                       package music.string;
+
+import music.Playable;
+
+public class Veena implements Playable {
+
+    @Override
+    public void play() {
+        System.out.println("Playing Veena");
+    }
+}
+
+
+
+
+3. Saxophone.java (Package: music.wind)
+
+
+
+                                         package music.wind;
+
+import music.Playable;
+
+public class Saxophone implements Playable {
+
+    @Override
+    public void play() {
+        System.out.println("Playing Saxophone");
+    }
+}
+
+
+4. Test.java (Package: live)
+
+
+                           package live;
+
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        // a. Create an instance of Veena and call play()
+        Veena veena = new Veena();
+        veena.play();
+
+        // b. Create an instance of Saxophone and call play()
+        Saxophone saxophone = new Saxophone();
+        saxophone.play();
+
+        // c. Use Playable reference
+        Playable p;
+
+        p = new Veena();
+        p.play();
+
+        p = new Saxophone();
+        p.play();
+    }
+}
+
+
+Output
+
+
+
+  Playing Veena
+Playing Saxophone
+Playing Veena
+Playing Saxophone
+                                 
